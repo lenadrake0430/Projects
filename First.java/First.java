66c8e819e42d8b463fc7fb1849c46ad032361bbd
lenadrake0430 <drake_helena@students.columbusstate.edu>
2026-01-26 22:24:53 -0500

    First Java
    Module 1 Assignment 1

---
+++ First.java/First.java
@@ -0,0 +1,16 @@
+
+/**
+ * Lena Johnson
+ * CPSC 1302K
+ * Module 1 Assignment 1
+ * Description:  My first java program about my future career plans with job and salary information
+ */
+public class First {
+    public static void main(String[] args) {
+        // Printing the required information
+        System.out.println("Hola mundo!");
+        System.out.println("Preferred Job title: Computer and Information Systems Managers");
+        System.out.println("Job Rationale: I am currently the a Telecommunications Manager, wanting to expand my career options.");
+        System.out.println("Median salary: $171,200");
+    }
+}
